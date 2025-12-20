0f706a2a2ec8a5f5ae9588c1c631f220400ef0e2
enchevri <enzochevrier@icloud.com>
2025-12-20 18:29:17 +0100

    Updated the exectuable name

---
+++ ex02/src/main.cpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: enchevri <enchevri@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/20 18:18:17 by enchevri          #+#    #+#             */
+/*   Updated: 2025/12/20 18:19:46 by enchevri         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <ostream>
+#include <string>
+
+int	main(void)
+{
+	std::string str = "HI THIS IS BRAIN";
+	std::string *stringPTR = &str;
+	std::string &stringREF = str;
+
+	std::cout << "Address of str:\t\t" << &str << std::endl;
+	std::cout << "Address of stringPTR:\t" << stringPTR << std::endl;
+	std::cout << "Address of stringREF:\t" << &stringREF << std::endl;
+	std::cout << "---------------------------------------------" << std::endl;
+	std::cout << "Address of str:\t\t" << str << std::endl;
+	std::cout << "Address of stringPTR:\t" << *stringPTR << std::endl;
+	std::cout << "Address of stringREF:\t" << stringREF << std::endl;
+	return (0);
+}
