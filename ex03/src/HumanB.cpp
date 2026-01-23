79e3110530c9b9b8d304956a8b5900c177359854
enchevri <enzochevrier@icloud.com>
2026-01-23 12:43:42 +0100

    fix: reorder member initializer list in HumanB constructor

--- ex03/src/HumanB.cpp
+++ ex03/src/HumanB.cpp
@@ -6,14 +6,14 @@
 /*   By: enchevri <enchevri@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/22 14:30:27 by enchevri          #+#    #+#             */
-/*   Updated: 2025/12/22 14:43:59 by enchevri         ###   ########lyon.fr   */
+/*   Updated: 2026/01/23 12:43:29 by enchevri         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <HumanB.hpp>
 #include <iostream>
 
-HumanB::HumanB(std::string name) : _weapon(NULL), _name(name)
+HumanB::HumanB(std::string name) : _name(name) , _weapon(NULL)
 {
 }
 
